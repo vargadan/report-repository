f000a294e60ceee0cea183f9b987c8da7cdbafc7
vargadan <danielvrg@gmail.com>
2017-09-18 01:14:13 +0200

    test

---
+++ src/test/java/net/vargadaniel/re/reportrepository/RepoTest.java
@@ -0,0 +1,14 @@
+package net.vargadaniel.re.reportrepository;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class RepoTest {
+	
+	@Test
+	public void testTrue() {
+		assertTrue(true);
+	}
+
+}
