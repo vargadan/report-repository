6d4c33a1f48be3f124634e1cda335d0ece65a244
vargadan <danielvrg@gmail.com>
2018-02-12 21:58:39 +0100

    change

--- src/main/java/net/vargadaniel/re/reportrepository/ReportEngine.java
+++ src/main/java/net/vargadaniel/re/reportrepository/ReportEngine.java
@@ -1,12 +1,18 @@
 package net.vargadaniel.re.reportrepository;
 
 import org.springframework.cloud.stream.annotation.Input;
+import org.springframework.cloud.stream.annotation.Output;
+import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.SubscribableChannel;
 
 public interface ReportEngine {
 	
-	String REPORT_FILES = "reportFiles"; 
+	final String REPORT_FILES = "reportFiles";
+	final String STATUS_UPDATES = "statusUpdates"; 
 	
 	@Input(REPORT_FILES)
 	SubscribableChannel reportFileTopic();
+	
+	@Output(STATUS_UPDATES)
+	MessageChannel statusUpdates();
 }
